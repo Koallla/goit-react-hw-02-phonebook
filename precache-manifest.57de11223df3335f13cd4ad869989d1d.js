b720312d2c8e82fd4592f1d9d870ac02d299c802
Koallla <zmiyov2422@gmail.com>
2019-12-20 18:44:54 +0200

    Updates

---
+++ precache-manifest.57de11223df3335f13cd4ad869989d1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01f64f296bcad3e75193ba93627de7e4",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "8554f4e39ea819fe21df",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.49fc8861.chunk.css"
+  },
+  {
+    "revision": "820b7f61fef66f8daab7",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.52085409.chunk.js"
+  },
+  {
+    "revision": "6b1e30fbae570017776d1838e82dbe98",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.52085409.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8554f4e39ea819fe21df",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.a8cf8a19.chunk.js"
+  },
+  {
+    "revision": "ae896622581a2a0a2520",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.a0dfc1a6.js"
+  }
+]);
